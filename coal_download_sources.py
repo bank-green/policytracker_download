e3853fb3e3a485622e261b63ca842a5a10bf4ffb
Albert Carter <albert.r.carter@gmail.com>
2023-12-11 14:27:15 +0100

    refactor coal for better history tracking

--- coal_download_sources.py
+++ coal_download_sources.py
@@ -1,64 +1,86 @@
 import os
 import pandas as pd
+import json
 import requests
 import re
 
-# Replace this with the path to your spreadsheet
-spreadsheet_path = 'coalpolicytracker.xlsx'  # or .csv
-output_folder = 'downloaded_content'
 
-# Read the spreadsheet
-df = pd.read_excel(spreadsheet_path)  # or pd.read_csv(spreadsheet_path) for CSV files
-
-# Ensure the output folder exists
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-
-# Iterate through the rows of the DataFrame
-output_dict = {}
-for index, row in df.iterrows():
-    text = row.iloc[-1]  # Last column for the URL
-    url_pattern = """https?://[^\\s]+"""
-    urls = re.findall(url_pattern, text)    
-    folder_name = row.iloc[2]  # 3rd column for the folder name
-    
-    output_dict[folder_name] = urls
+def clean_filename(filename):
+    return re.sub(r'[\/*?:"<>|]', "", filename)  # Remove invalid characters
 
 
-    # # Create a folder for each unique name in the 3rd column
-    # folder_path = os.path.join(output_folder, str(folder_name))
-    # if not os.path.exists(folder_path):
-    #     os.makedirs(folder_path)
-    
-    # for url in urls:
-    #     try:
-    #     # Download the content from the URL
-    #         headers = {
-    #         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/117.0"
-    #         }
-    #         response = requests.get(headers=headers, url=url)
-    #         response.raise_for_status()
+def download_url(url, output_folder, bank_name, output_dict):
+    try:
+        # Download the content from the URL
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/117.0"
+        }
+        response = requests.get(headers=headers, url=url, timeout=60)
+        response.raise_for_status()
 
-    #     # Assuming the URL points to a file, extract filename
-        
-    #         if url.endswith('/'):
-    #             url = url[:-1]
-            
-    #         filename = url.split('/')[-1]
-    #         file_path = os.path.join(folder_path, filename)
+        # Assuming the URL points to a file, extract filename
+        if url.endswith("/"):
+            url = url[:-1]
 
-    #         # Write the content to a file
-    #         with open(file_path, 'wb') as file:
-    #             file.write(response.content)
+        filename = clean_filename(url.split("/")[-1])
+        file_path = os.path.join(output_folder, bank_name, filename)
 
-    #         print(f'Downloaded {url} into {folder_path}')
+        # Write the content to a file
+        with open(file_path, "wb") as file:
+            file.write(response.content)
+
+        output_dict[bank_name]["output"].append(
+            {"url": url, "actual_url": url, "downloaded": True}
+        )
+
+        # print
+        print(f"✅ {file_path}")
+
+    except requests.RequestException as e:
+        output_dict[bank_name]["output"].append(
+            {"url": url, "actual_url": None, "downloaded": False}
+        )
+        print(f"🚨 {bank_name}/{url}: {e}")
+
+    return output_dict
 
 
-    #     except requests.RequestException as e:
-    #         print(f'Error downloading {url}: {e}')
+def download_coalpolicytracker():
+    spreadsheet_path = "coalpolicytracker.xlsx"  # or .csv
+    output_folder = "downloaded_content"
+    df = pd.read_excel(spreadsheet_path)  # Read the spreadsheet
+    output_dict = {}
 
-# print('Download completed.')
-import json
+    with open("./downloaded_content/tags.json") as f:
+        tags_dict = json.load(f)
 
-with open ("downloaded_content/urls.json", "w") as f:
-    json.dump(output_dict, f)
+    # Ensure the output folder exists
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Iterate through the rows of the DataFrame
+    for _, row in df.iterrows():
+        text = row.iloc[-1]  # Last column for the URL
+        url_pattern = """https?://[^\\s]+"""
+        urls = re.findall(url_pattern, text)
+        bank_name = row.iloc[2]  # 3rd column for the folder name
+
+        output_dict[bank_name] = {}
+        output_dict[bank_name]["tag"] = tags_dict[bank_name]
+        output_dict[bank_name]["output"] = []
+
+        # Create a folder for each unique name in the 3rd column
+        folder_path = os.path.join(output_folder, str(bank_name))
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        for url in urls:
+            output_dict = download_url(url, output_folder, bank_name, output_dict)
+
+        with open("downloaded_content/_urls.json", "w") as f:
+            json.dump(output_dict, f, indent=4)
+
+    print("Download completed.")
+
+
+download_coalpolicytracker()
